b06f052b3f105a714b52653cf0ea77d37dc38e46
pure <64907372+mirlindxd@users.noreply.github.com>
2021-06-26 21:12:56 +0200

    Create minecraftInstaller.sh
    A Script used to install Minecraft 1.17 on Debian based OS!
    
    Not done: Automatically Java Installation and folder/files check if it exists

---
+++ minecraftInstaller.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root and not as sudo!"
+  exit
+fi
+echo "installing ufw for opening ports for the minecraft server and other needed tools..."
+apt install ufw wget -y
+ufw allow 25565
+apt update && apt upgrade -y
+echo done installed dependencies
+cd /home/$username
+mkdir minecraft
+cd minecraft
+echo downloading now minecraft paperspigot 1.17...
+wget https://papermc.io/api/v2/projects/paper/versions/1.17/builds/49/downloads/paper-1.17-49.jar
+echo done
+touch eula.txt
+echo eula=true >> eula.txt
+touch startMC.sh && echo java -Xmx1024M -Xms1024M -jar paper-1.17-49.jar >> startMC.sh
+bash startMC.sh
